0df6df8d5b1b9a33cc3490447e70766d70fc2604
Florence Hapgood <flohapgood@icloud.com>
2019-01-30 14:17:13 +0000

    Tests for error message

--- cypress/integration/weapons.spec.js
+++ cypress/integration/weapons.spec.js
@@ -5,12 +5,19 @@ describe('Arm me button', function() {
     cy.get('#1').contains('Weapon: Sword')
     cy.get('#1').should('not.contain', 'Weapon: Unarmed')
   })
-})
 
-describe('Arm me button', function() {
+
   it ('Takes a weapon from the Arsenal', function() {
     cy.visit('/')
     cy.get('#1').find('.Sword').click()
     cy.get('.arsenal').should('not.contain', 'Sword')
+  })
+
+  it ('Will not allocated the same weapon twice', function() {
+    cy.visit('/')
+    cy.get('#1').find('.Sword').click()
+    cy.get('#2').find('.Sword').click()
+    cy.get('#2').should('not.contain', 'Weapon:Sword')
+    cy.get('#2').should('contain', 'You have already given this weapon to another character')
   })
 })
