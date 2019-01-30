a3283c177c0e271666821b8e8bcd5f405191fd52
Florence Hapgood <flohapgood@icloud.com>
2019-01-30 14:13:30 +0000

    Test for Arsenal
    Tested that weapon gets taken from Arsenal

--- cypress/integration/weapons.spec.js
+++ cypress/integration/weapons.spec.js
@@ -1,8 +1,16 @@
 describe('Arm me button', function() {
   it ('Allocates a weapon to the character', function() {
     cy.visit('/')
-    cy.get('#1').find('.Sword').click()  
+    cy.get('#1').find('.Sword').click()
     cy.get('#1').contains('Weapon: Sword')
     cy.get('#1').should('not.contain', 'Weapon: Unarmed')
+  })
+})
+
+describe('Arm me button', function() {
+  it ('Takes a weapon from the Arsenal', function() {
+    cy.visit('/')
+    cy.get('#1').find('.Sword').click()
+    cy.get('.arsenal').should('not.contain', 'Sword')
   })
 })
